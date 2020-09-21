ea6405189041c91ba11beb8f27778e1d8c013fa5
darekbx <darek.baranczuk@gmail.com>
2020-09-21 12:12:47 +0200

    Increase level of difficult

--- app/src/main/java/com/darekbx/mathgames/ui/equations/EquationGenerator.kt
+++ app/src/main/java/com/darekbx/mathgames/ui/equations/EquationGenerator.kt
@@ -1,5 +1,6 @@
 package com.darekbx.mathgames.ui.equations
 
+import kotlin.math.max
 import kotlin.random.Random
 
 class EquationGenerator {
@@ -9,15 +10,21 @@ class EquationGenerator {
 
     fun generate(level: Int): String {
         val maximum = initialValue + level
-        val firstNumber = randomInt(maximum)
-        val secondNumber = randomInt(maximum)
-        val thirdNumber = randomInt(maximum)
+
+        val minimumFirst = level - 1
+        val minimumSecond = level + 1
+        val minimumThird = level
+
+        val firstNumber = randomInt(minimumFirst, maximum)
+        val secondNumber = randomInt(minimumSecond, maximum)
+        val thirdNumber = randomInt(minimumThird, maximum)
         val firstModificator = randomModificator()
         val secondModificator = randomModificator()
+
         return "$firstNumber $firstModificator $secondNumber $secondModificator $thirdNumber"
     }
 
     private fun randomModificator() = modificators[Random.nextInt(modificators.size)]
 
-    private fun randomInt(maximum: Int) = Random.nextInt(1, maximum)
+    private fun randomInt(minimum: Int, maximum: Int) = Random.nextInt(max(minimum, 1), minimum + maximum)
 }
